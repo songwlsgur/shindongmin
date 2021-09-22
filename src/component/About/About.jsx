c8922b63a53fbe4e945486d09a3654cc4a5b3a29
songjinhyeok <songwlsgur@gmail.com>
2021-09-22 18:41:17 +0900

    about 수정

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -9,22 +9,39 @@ import { useEffect } from "react";
 import sample7 from "../../images/sample_images_07.png";
 import "../../css/total.css";
 import "../IndexCheck/indexCheck.css";
+import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
 function About(props) {
   return (
     <>
-      <div className="mainContent">
-        <div id="intro">
-          <div className="introContent">
-            <img src={sample7} alt="test사진" />
+      <SimpleReactLightbox>
+        <SRLWrapper>
+          <div className="content-exp index2">
+            <p className="content-title mb-5">About Explanation</p>
+            <div>
+              Dongmin Shin is 29 years old. <br />
+              I currently live in Seoul, Korea.
+              <br />
+              We will be running towards our future dreams.
+              <br />
+              Thank you so much for your support.
+              <br />
+            </div>
           </div>
-          <div className="introContent">
-            <img src={sample7} alt="test사진" />
+          <div className="mainContent">
+            <div id="intro">
+              <div className="introContent">
+                <img src={sample7} alt="sample7 사진" />
+              </div>
+              <div className="introContent">
+                <img src={sample7} alt="sample7 사진" />
+              </div>
+              <div className="introContent">
+                <img src={sample7} alt="sample7 사진" />
+              </div>
+            </div>
           </div>
-          <div className="introContent">
-            <img src={sample7} alt="test사진" />
-          </div>
-        </div>
-      </div>
+        </SRLWrapper>
+      </SimpleReactLightbox>
     </>
   );
 }
